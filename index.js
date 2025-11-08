d1a548fef927df3a9902f8adda0af3ced2bf222b
Rifat240510 <rifat240510@gmail.com>
2025-11-09 01:42:39 +0600

    connected with mongodb and data added in mongodb by insert

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = 3000
+app.use(cors())
+app.use(express.json())
 
 
-app.use(cors());
+
+
+const uri = "mongodb+srv://habit-db:WAxSAyNcM1RuLQsL@cluster0.qwnp7az.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+
+
+
+
+
+    
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World')
